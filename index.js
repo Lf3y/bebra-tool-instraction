2a420295a595fd3026a2b90967b5bb77e3070ce9
Lf3y <lfey6677@gmail.com>
2025-03-25 18:43:58 +0300

    Update slider

--- index.js
+++ index.js
@@ -124,3 +124,25 @@ const translations = {
                 }, 500);
             }, 800); // Прелоадер исчезает через 0.8 секунды
         });
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle("active", i === index);
+    });
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
+    showSlide(currentSlide);
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
